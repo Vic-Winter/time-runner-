443181efe3b48e89cdc2fd1106da4eefd3c688d9
Vic Winter <messia0504@gmail.com>
2017-03-20 20:36:35 +0100

    Start on unit test.

---
+++ src/test/java/com/timeSlides/timeSlideIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.timeSlides;
+
+/**
+ * Created by yeleilu on 20/03/2017.
+ */
+public class timeSlideIntegrationTest
+{
+}
---
+++ src/test/java/com/user/userIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.user;
+
+/**
+ * Created by yeleilu on 20/03/2017.
+ */
+public class userIntegrationTest
+{
+}
